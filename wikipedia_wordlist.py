2d6a08e95483e71eac33040f4ea848052bbf24f4
Hugh Williams <hughaw64@gmail.com>
2019-01-12 14:15:19 +0000

    Fixed memory leak

--- wikipedia_wordlist.py
+++ wikipedia_wordlist.py
@@ -4,28 +4,22 @@ import re
 import os
 
 def get_articles(path):
-    parser = ET.XMLPullParser(['start', 'end'])
     tag_regex = re.compile('{.*?}(.*)')
-    with open(path, 'rb') as file:
-        title = ''
-        text = ''
-        data = '1'
-        while data:
-            data = file.read(8192).decode('utf-8', 'ignore')
-            parser.feed(data)
-            for event, elem in parser.read_events():
-                tag = tag_regex.match(elem.tag).group(1)
-                if tag == 'page':
-                    if event == 'end':
-                        if '#REDIRECT' not in text:
-                            yield title, text
-                        title = ''
-                        text = ''
-                elif tag == 'title':
-                    title = elem.text
-                elif tag == 'text':
-                    if elem.text and event=='end':
-                        text += elem.text
+    title = ''
+    text = ''
+    for event, elem in ET.iterparse(path, events=('end', )):
+        tag = tag_regex.match(elem.tag).group(1)
+        if tag == 'page':
+            if '#REDIRECT' not in text:
+                yield title, text
+            title = ''
+            text = ''
+        elif tag == 'title':
+            title = elem.text
+        elif tag == 'text':
+            if elem.text:
+                text += elem.text
+        elem.clear()
 
 def get_meta(article):
     meta = {}
@@ -35,22 +29,15 @@ def get_meta(article):
 
 def word_count(path, frequency=100):
     words = collections.Counter()
-    with open('wiki_words.txt') as file:
-        old_words = dict(map(lambda x: x.split(','),
-                         file.read().lower().splitlines()))
-    for i in old_words:
-        words[i] = int(old_words[i])
-    position = int(input('Position>>>'))
+    position = 0
     word_regex = re.compile('(?:(?<=^)|(?<=\s))([A-Za-z]+)(?:(?=$)|(?=\s))')
     for title, text in get_articles(path):
         position += 1
         words.update(word_regex.findall(text))
-        
-        
         if position % frequency == 0:
-            print('Position:', position)
+            print('Position:', position, 'Words:', len(words))
             yield words
-                        
+
 
 def save_word_wordcount(path, frequency=100):
     for current_result in word_count(path, frequency):
@@ -69,4 +56,4 @@ def save_word_wordcount(path, frequency=100):
 if __name__ == '__main__':
     path = (r'D:\Downloads\enwiki-20181020-pages-articles-multistream.xml'
             '\enwiki-20181020-pages-articles-multistream.xml')
-    save_word_wordcount(path, 100)
+    save_word_wordcount(path, 200)
